afa4a2f3c375604677c059e849a5bae654421f83
Ayush05m <ayush05m@gmail.com>
2025-09-04 12:19:02 +0530

    updates

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -11,7 +11,7 @@ export const api = axios.create({
 
 // Add interceptors for token handling
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('authToken');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
